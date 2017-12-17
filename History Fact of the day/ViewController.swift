e6bf23f2f6517a793041224591a8bf6d5431eb91
dav-jordan <davisridleyjordan@gmail.com>
2017-12-17 10:42:19 -0600

    First commit

---
+++ History Fact of the day/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  History Fact of the day
+//
+//  Created by Davis Jordan on 12/17/17.
+//  Copyright © 2017 Davis Jordan. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+
+
+
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+*Not finished yet*
+
+This is a simple ine page app that sends the user one history fact every day.
